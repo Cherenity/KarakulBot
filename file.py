ff3a574e14808f0d635b2b636edd2569eef5b757
Cherenity <jonna_moilanen@hotmail.com>
2023-03-30 18:06:07 +0300

    Initial commit

---
+++ file.py
@@ -0,0 +1,36 @@
+import discord
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+# Connection to the discord:
+client = discord.Client(intents=intents)
+
+# Called when bot is ready for use:
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        await message.channel.send('Hello!')
+
+
+# Main program
+def main():
+    print("--main program--")
+    print(os.getenv('token'))
+ 
+    #client.run(token)
+
+
+
+# Defining main program
+if __name__ == "__main__":
+    main()
